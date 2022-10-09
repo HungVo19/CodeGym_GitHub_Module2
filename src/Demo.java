0e8922ded01985a3484e042253574ba18005d5eb
VoHung19 <vohung193@gmail.com>
2022-10-09 08:39:23 +0700

    upload

--- src/Demo.java
+++ src/Demo.java
@@ -5,15 +5,8 @@ import java.util.Scanner;
 
 public class Demo {
     public static void main(String[] args) {
-        int[] array = {1, 2, 3, 4, 5};
-        int value = 6;
-        int indexToAdd = 6;
+        Scanner input = new Scanner(System.in);
 
-        int[] newArray = new int[array.length+Math.abs(indexToAdd - array.length+1)];
-        for (int i =0 ; i < array.length; i++){
-            newArray[i] = array[i];
-        }
-        newArray[newArray.length-1] = value;
-        System.out.print(Arrays.toString(newArray));
+
     }
 }
